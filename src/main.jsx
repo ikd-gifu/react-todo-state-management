57c8dbb1cad91a2651d5ed6b94f8aaa83f8d295e
ikd-gifu <oki19ki@gmail.com>
2026-01-06 18:09:14 +0900

    Topページ表示

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' // React 18 以降の新しいレンダリング方法
+import { TodoPage } from "./pages";
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodoPage />
   </StrictMode>,
 )
---
+++ src/pages/TodoPage/TodoPage.jsx
@@ -0,0 +1,8 @@
+export const TodoPage = () => {
+  return (
+    <div>
+      <h1>Todo アプリ</h1>
+      <p>これから Todo 機能を実装します</p>
+    </div>
+  );
+};
